6a12763d2e24ae8b87c2ce48b7a1cde732a11ee2
BaoHG <baohuynh291@gmail.com>
2024-05-04 14:14:17 +0700

    add
    add

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class LinearEquationSolver
+{
+    static void Main()
+    {
+        Console.Write("Nhap a: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Nhap b: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine($"Phuong trinh cua ban la: {a}x + {b} = 0");
+        if (a == 0)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("Phuong trinh co nghiem");
+            }
+            else
+            {
+                Console.WriteLine("Phuong trinh vo nghiem");
+            }
+        }
+        else
+        {
+            double x = -b / a;
+            Console.WriteLine($"Nghiem cua phuong trinh x = {x}");
+        }
+    }
+}
